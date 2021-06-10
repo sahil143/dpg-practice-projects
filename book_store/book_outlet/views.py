e60e4a527acf12a00d33d785259020e2f9a1bbf9
Sahil Budhwar <sahilbudhwar143@gmail.com>
2021-06-10 13:36:44 +0530

    learn about models by creating model for books in book_store app

---
+++ book_store/book_outlet/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import get_object_or_404, render
+from .models import Book
+from django.http import Http404
+from django.db.models import Avg
+# Create your views here.
+
+def index(request):
+  books = Book.objects.all()
+  number_of_books = books.count()
+  avg_rating = books.aggregate(Avg("rating"))
+  return render(request, 'book_outlet/index.html', {
+    "books": books,
+    "total_number_of_books": number_of_books,
+    "average_rating": avg_rating,
+    })
+
+def book_detail(request, slug):
+  book = get_object_or_404(Book, slug=slug)
+  return render(request, 'book_outlet/book_detail.html', {
+    'title': book.title,
+    'author': book.author,
+    'rating': book.rating,
+    'is_bestselling': book.is_bestselling
+  })
