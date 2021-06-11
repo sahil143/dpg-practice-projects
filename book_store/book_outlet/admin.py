e70f0259deff1c30cbf7f19981d8fb1f06047fb4
Sahil Budhwar <sahilbudhwar143@gmail.com>
2021-06-11 11:18:27 +0530

    relationsships oneTOmany, oneToOne

--- book_store/book_outlet/admin.py
+++ book_store/book_outlet/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Address, Book, Author
+class BookAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("title",)}
+    list_filter = ("author", "rating")
+    list_display = ("title", "author")
+
+
+admin.site.register(Book, BookAdmin)
+admin.site.register(Author)
+admin.site.register(Address)
--- book_store/book_outlet/models.py
+++ book_store/book_outlet/models.py
@@ -5,14 +5,42 @@ from django.urls.base import reverse
 from django.utils.text import slugify
 # Create your models here.
 
+class Address(models.Model):
+    street = models.CharField(max_length=80)
+    postal_code = models.CharField(max_length=5)
+    city = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"{self.street}, {self.city}, {self.postal_code}"
+    class Meta:
+        verbose_name_plural = "Address Enteries"
+    
+
+
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    address = models.OneToOneField(Address, on_delete=models.CASCADE, null=True)
+
+    def full_name(self):
+        return f"{self.first_name} ({self.last_name})"
+
+    def __str__(self):
+        return self.full_name()
+
+    def get_absolute_url(self):
+        return reverse("author_detail", kwargs={"pk": self.pk})
+
+
 
 class Book(models.Model):
     title = models.CharField(max_length=50)
     rating = models.IntegerField(
         validators=[MinValueValidator(1), MaxValueValidator(5)])
-    author = models.CharField(null=True, max_length=100)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, null=True, related_name="books")
     is_bestselling = models.BooleanField(default=False)
-    slug = models.SlugField(default='', null=False, db_index=True)
+    slug = models.SlugField(default='', blank=True, null=False, db_index=True,)
 
     def get_absolute_url(self):
         return reverse("book-detail", args=[self.slug])
