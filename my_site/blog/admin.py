904b34feaf523dc9cc0e77a63412aba04f42c23e
Sahil Budhwar <sahilbudhwar143@gmail.com>
2021-06-11 12:19:58 +0530

    update blog to used models and database

--- my_site/blog/admin.py
+++ my_site/blog/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Post, Author, Tag
+
+class PostAdmin(admin.ModelAdmin):
+  list_display = ("title", "author", "date",)
+  list_filter = ("author", "tags", "date",)
+  prepopulated_fields = {"slug": ("title",)}
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Author)
+admin.site.register(Tag)
+
